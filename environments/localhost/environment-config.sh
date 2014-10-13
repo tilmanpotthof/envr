987454f92aa513741947cd7665820237c53912df
Tilman Potthof <tilmanpotthof@googlemail.com>
2014-10-13 11:40:39 +0200

    added piwik url to example.

--- environments/localhost/environment-config.sh
+++ environments/localhost/environment-config.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 WORDPRESS_URL="localhost\/my-wordpress"
+PIWIK_URL="localhost\/my-piwik"
 DB_AUTH="-u root -S /Applications/XAMPP/xamppfiles/var/mysql/mysql.sock"
 DB_NAME='my_wordpress_db'
