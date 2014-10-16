67aa8bb55303b3f0fccd92d13d5be7e1a120b178
Tilman Potthof <tilmanpotthof@googlemail.com>
2014-10-16 23:04:50 +0200

    improved db normalization: placeholder for db variables can be replaced in php-serialize-format without 'http://' as prefix.

--- scripts/normalize-sql.sh
+++ scripts/normalize-sql.sh
@@ -33,14 +33,14 @@ do
 		PLACEHOLDER="{{{$VARIABLE_NAME}}}"
 
 		## step to replace urls that are stores in non-atomic fields with phps serialize function
-		for MATCH in `perl -wnE 'print $_ =~ /s:[0-9]+:\\\"http:\/\/'$PLACEHOLDER'.*?\";/g' $CURRENT_ENVIRONMENT_PATH/$LATEST_SQL | perl -pne 's/;s:/;\ns:/g'`
+		for MATCH in `perl -wnE 'print $_ =~ /(s:[0-9]+:\\\"h?t?t?p?:?\/?\/?'$PLACEHOLDER'.*?\";)/g' $CURRENT_ENVIRONMENT_PATH/$LATEST_SQL | perl -pne 's/;s:/;\ns:/g'`
 		do
-			URL=`echo $MATCH | perl -ne '$_ =~ /\\\"(.*)\\\"/g;print $1' | perl -pne 's/'$PLACEHOLDER'/'$VALUE'/g'`
+			URL=`echo $MATCH | perl -ne '$_ =~ /\\\"(.*)\\\"/g;print $1' | perl -pne 's!'$PLACEHOLDER'!'$VALUE'!g'`
 			LENGTH=${#URL}
 			REPLACEMENT='s:'$LENGTH':\\\"'$URL'\\\";'
 
 			# escape backslash in MATCH
-			ESCAPED_MATCH=`echo $MATCH | perl -pne 's!\\\\!\\\\\\\\!g'`
+			ESCAPED_MATCH=`echo $MATCH | perl -pne 's!\\\\!\\\\\\\\!g' | perl -pne 's!\?!\\\?!g'`
 
 			perl -pi -e 's!'$ESCAPED_MATCH'!'$REPLACEMENT'!g' $CURRENT_ENVIRONMENT_PATH/$LATEST_SQL
 		done
