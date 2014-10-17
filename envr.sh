2688c30e60e9abc1a0d7d372f362d4be0bcdc513
Tilman Potthof <tilmanpotthof@googlemail.com>
2014-10-17 11:57:51 +0200

    basic install script (fixes #3).

--- envr.sh
+++ envr.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
-./scripts/envr.sh $0 $1 $2
+ENVR_CONFIG='.envr_config'
+
+if [ -f $ENVR_CONFIG ]; then
+	source $ENVR_CONFIG
+else
+	echo "WARNING: Found no $ENVR_CONFIG file. Please use the install.sh script."
+	exit 0
+fi
+
+./$ENVR_PATH/scripts/envr.sh $0 $1 $2
---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+ENVR_PATH="${0%/*}"
+
+ENVR_CONFIG='.envr_config'
+ENVR_CONFIG_SAMPLE=$ENVR_PATH/$ENVR_CONFIG'.template'
+
+if [ ! -f $ENVR_CONFIG ]; then
+	while [[ ! $INSTALL_HERE =~ ^[YyNn]$ ]]
+	do
+	    read -p "Install envr in this folder $(pwd)? [y/n] " -n 1 -r INSTALL_HERE
+	    echo ""
+	done
+	if [[ $INSTALL_HERE =~ ^[Nn]$ ]]
+    then
+        echo "Switch to the folder where you want do install envr and run envr install.sh again."
+        exit 0
+    fi
+
+	eval echo "\"$(cat $ENVR_CONFIG_SAMPLE)\"" > $ENVR_CONFIG
+	echo "✓ created $ENVR_CONFIG file"
+else
+	echo "WARNING: $ENVR_CONFIG file already exists. Delete it before you want to reinstall envr."
+	exit 0
+fi
+
+cp $ENVR_PATH/envr.sh ./
+echo "✓ copied envr script to current directory"
