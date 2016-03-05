c239f44802db238c5bb2c58c9f6a6e68826b68ea
Tilman Potthof <tilmanpotthof@googlemail.com>
2016-03-05 17:39:00 +0100

    create folders for environment dependent sql dumps

--- scripts/normalize-sql.sh
+++ scripts/normalize-sql.sh
@@ -25,7 +25,9 @@ for ENVIRONMENT in `ls $ENVIRONMENTS_DIR`
 do
 	source ./$SCRIPTS/prepare-environment.sh $ENVIRONMENT
 	echo "create sql for $ENVIRONMENT with $WORDPRESS_URL"
-	cp $LATEST_SQL $CURRENT_ENVIRONMENT_PATH/$LATEST_SQL
+	ENVIRONMENT_SQL=$CURRENT_ENVIRONMENT_PATH/$LATEST_SQL
+	mkdir -p $(dirname $ENVIRONMENT_SQL)
+	cp $LATEST_SQL $ENVIRONMENT_SQL
 
 	for VARIABLE_NAME in $(cat $DB_VARIABLES)
 	do
@@ -33,7 +35,7 @@ do
 		PLACEHOLDER="{{{$VARIABLE_NAME}}}"
 
 		## step to replace urls that are stores in non-atomic fields with phps serialize function
-		for MATCH in `perl -wnE 'print $_ =~ /(s:[0-9]+:\\\"h?t?t?p?:?\/?\/?'$PLACEHOLDER'.*?\";)/g' $CURRENT_ENVIRONMENT_PATH/$LATEST_SQL | perl -pne 's/;s:/;\ns:/g'`
+		for MATCH in `perl -wnE 'print $_ =~ /(s:[0-9]+:\\\"h?t?t?p?:?\/?\/?'$PLACEHOLDER'.*?\";)/g' $ENVIRONMENT_SQL | perl -pne 's/;s:/;\ns:/g'`
 		do
 			URL=`echo $MATCH | perl -ne '$_ =~ /\\\"(.*)\\\"/g;print $1' | perl -pne 's!'$PLACEHOLDER'!'$VALUE'!g'`
 			LENGTH=${#URL}
@@ -42,10 +44,10 @@ do
 			# escape backslash in MATCH
 			ESCAPED_MATCH=`echo $MATCH | perl -pne 's!\\\\!\\\\\\\\!g' | perl -pne 's!\?!\\\?!g'`
 
-			perl -pi -e 's!'$ESCAPED_MATCH'!'$REPLACEMENT'!g' $CURRENT_ENVIRONMENT_PATH/$LATEST_SQL
+			perl -pi -e 's!'$ESCAPED_MATCH'!'$REPLACEMENT'!g' $ENVIRONMENT_SQL
 		done
 
-		perl -pi -e 's!'$PLACEHOLDER'!'$VALUE'!g' $CURRENT_ENVIRONMENT_PATH/$LATEST_SQL
+		perl -pi -e 's!'$PLACEHOLDER'!'$VALUE'!g' $ENVIRONMENT_SQL
 
 	done
 done
