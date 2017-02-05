d2cfbce09fd65a836c9e510b2814db74867c2308
Tilman Potthof <tilmanpotthof@googlemail.com>
2017-02-05 19:05:03 +0100

    Fix normalize function for environments with php (cgi-fcgi)

--- scripts/normalize-sql.sh
+++ scripts/normalize-sql.sh
@@ -3,6 +3,7 @@
 source ./$SCRIPTS/prepare-environment.sh
 
 DB_VARIABLES=".db_variables"
+TMP_PHP_FILE="___.php"
 
 
 if [ ! -f $DB_VARIABLES ]; then
@@ -21,6 +22,19 @@ done
 
 echo "✓ done preparing all environments"
 
+PHP_CLI=$(php --version | grep "(cli)" | wc -l)
+
+function replace {
+    PHP_COMMAND='print serialize("'$1'");'
+
+    if [ $PHP_CLI == 1 ]; then
+        php -r "$PHP_COMMAND"
+    else
+        echo "<?php "$PHP_COMMAND > $TMP_PHP_FILE
+        php -f $TMP_PHP_FILE
+    fi
+}
+
 for ENVIRONMENT in `ls $ENVIRONMENTS_DIR`
 do
 	source ./$SCRIPTS/prepare-environment.sh $ENVIRONMENT
@@ -38,7 +52,7 @@ do
 		for MATCH in `perl -wnE 'print $_ =~ /(s:[0-9]+:\\\"h?t?t?p?:?\/?\/?'$PLACEHOLDER'.*?\";)/g' $ENVIRONMENT_SQL | perl -pne 's/;s:/;\ns:/g'`
 		do
 			URL=`echo $MATCH | perl -ne '$_ =~ /\\\"(.*)\\\"/g;print $1' | perl -pne 's!'$PLACEHOLDER'!'$VALUE'!g'`
-			REPLACEMENT=$(php -r 'print serialize("'$URL'");' )
+			REPLACEMENT=$(replace $URL)
 
 			# escape backslash in MATCH
 			ESCAPED_MATCH=`echo $MATCH | perl -pne 's!\\\\!\\\\\\\\!g' | perl -pne 's!\?!\\\?!g'`
@@ -50,6 +64,8 @@ do
 
 	done
 done
+
+rm -r $TMP_PHP_FILE
 
 echo "✓ done creating sql for all environments"
 
